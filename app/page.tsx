cd449443a1ab54e7c84845211890f23b20ad3e08
Jayesh Shrivastava <jayeshshrivastava815@gmail.com>
2025-06-18 10:24:08 +0530

    SSR fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { navItems } from "@/data";
+import ClientOnly from "@/components/ui/ClientOnly";
 
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
@@ -14,20 +15,22 @@ import ScrollToTop from "@/components/ScrollToTop";
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 scroll-smooth">
-      <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
-        <div style={{ height: "60px" }} /> {/* Spacer for navbar */}
-        <Hero />
-        <Grid />
-        <RecentProjects />
-        <Experience />
-        <TechStack />
-        <CodingProfile />
-        <Footer />
-        <ScrollToTop />
-      </div>
-    </main>
+    <ClientOnly>
+      <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 scroll-smooth">
+        <div className="max-w-7xl w-full">
+          <FloatingNav navItems={navItems} />
+          <div style={{ height: "60px" }} /> {/* Spacer for navbar */}
+          <Hero />
+          <Grid />
+          <RecentProjects />
+          <Experience />
+          <TechStack />
+          <CodingProfile />
+          <Footer />
+          <ScrollToTop />
+        </div>
+      </main>
+    </ClientOnly>
   );
 };
 
