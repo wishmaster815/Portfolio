56a51ebefb780b97724eb46979010900671fc09b
Jayesh Shrivastava <jayeshshrivastava815@gmail.com>
2025-11-29 19:34:11 +0530

    cursor animation

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,12 @@ import { FloatingNavbar } from "@/components/ui/FloatingNavbar";
 import TechStack from "@/components/TechStack";
 import CodingProfile from "@/components/CodingProfile";
 import ScrollToTop from "@/components/ScrollToTop";
+import SplashCursor from "@/components/ui/SplashCursor";
 
 const Home = () => {
   return (
     <ClientOnly>
+      <SplashCursor BACK_COLOR={{ r: 0.1, g: 0.5, b: 0.2 }} />
       <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 scroll-smooth">
         <div className="max-w-7xl w-full">
           <FloatingNavbar navItems={navItems} />
